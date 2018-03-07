1d79815504111b5d057b69b6e66603e2ae8244f3
Bearsome <milewski2000@wp.pl>
2018-03-07 22:31:42 +0100

    tweaked speed values, all values now editable in editor without changing code

--- characters/player.gd
+++ characters/player.gd
@@ -1,25 +1,27 @@
 extends KinematicBody2D
 
+export var running_speed = 0
+export var jumping_speed = 0
+export var gravity = 0
+
 var motion = Vector2()
-export var player_speed_x = 100
-export var player_speed_y = -400
 const UP = Vector2(0, -1)
 
 #basic left/right movement
 
 func _process(delta):
-	motion.y += 10
+	motion.y += gravity
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = player_speed_x
+		motion.x = running_speed
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -player_speed_x
+		motion.x = -running_speed
 	else:
 		motion.x = 0
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
-			motion.y = player_speed_y
+			motion.y = jumping_speed
 	
 	#always affect character with values
 	motion = move_and_slide(motion, UP)
