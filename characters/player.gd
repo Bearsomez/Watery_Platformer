ed355876a90bf3905d0046d15f5915931078ddbf
Bearsome <milewski2000@wp.pl>
2018-03-11 14:20:30 +0100

    added example tileset, platforming improvements
    example set in the project, more so to see how to make a one
    
    platforming now has friction and acceleration

--- characters/player.gd
+++ characters/player.gd
@@ -1,29 +1,40 @@
 extends KinematicBody2D
-
-export var running_speed = 0
+#defining variables
+export var max_running_speed = 0
 export var jumping_speed = 0
 export var gravity = 0
-
+export var acceleration = 0
+export(float, -1, 1) var friction = 0
+#basic movement definitions
 var motion = Vector2()
 const UP = Vector2(0, -1)
-
-#basic left/right movement
 
 func _process(delta):
 	motion.y += gravity
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = running_speed
+		#movement
+		motion.x = min(motion.x+acceleration, max_running_speed)
+		print(motion.x)
+		#animations
+		$Sprite.flip_h = false
+		$Sprite.play("walk")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -running_speed
+		#movement
+		motion.x = max(motion.x-acceleration, -max_running_speed)
+		#animations
+		$Sprite.flip_h = true
+		$Sprite.play("walk")
 	else:
-		motion.x = 0
+		$Sprite.play("idle")
+		motion.x = lerp(motion.x, 0, friction)
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
-			motion.y = jumping_speed
+			motion.y = -jumping_speed
+	else:
+		$Sprite.play("jump")
 	
 	#always affect character with values
 	motion = move_and_slide(motion, UP)
-	print(motion)
 	pass
